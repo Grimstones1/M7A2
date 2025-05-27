0fd96123c34aa40c22a4a155e8cbfa3679699f3b
BuildTools <unconfigured@null.spigotmc.org>
2025-05-27 16:46:36 -0700

    ✅ Commit message: Stage 3: Use template string with placeholders for story

--- src/MadLib.java
+++ src/MadLib.java
@@ -4,7 +4,11 @@ import static java.lang.System.out;
 
 public class MadLib {
     public static void main(String[] args) {
+
         out.println("Mad Lib!");
+        String template = "The {adjective} {noun} {adverb} {verb}.";
+        String template1 = "The {adjective} {noun} will {verb} {adverb}.";
+        out.println(template);
 
         Scanner input = new Scanner(System.in);
         out.println("Enter an adjective (e.g., red, bright, smart):");
@@ -16,7 +20,18 @@ public class MadLib {
         out.println("Enter an adverb (e.g., viciously, brightly, sometimes):");
         String adverb = input.nextLine();
         input.close();
-
-        out.println("The " + adjective + " " + noun + " " + verb + " " + adverb);
+        template = replaceStringPlaceHolders(template, adjective, noun, verb, adverb);
+        template1 = replaceStringPlaceHolders(template1, adjective, noun, verb, adverb);
+        out.println(template);
+        out.println(template1);
     }
+
+    static String replaceStringPlaceHolders(String input, String adjective, String noun, String verb, String adverb) {
+        input = input.replace("{adjective}", adjective);
+        input = input.replace("{noun}", noun);
+        input = input.replace("{verb}", verb);
+        input = input.replace("{adverb}", adverb);
+        return input;
+    }
+
 }
