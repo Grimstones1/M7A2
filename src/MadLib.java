422c98680f4a150eb7817fab95bc5a3a1c690238
BuildTools <unconfigured@null.spigotmc.org>
2025-05-27 15:35:28 -0700

    ✅ Commit message: Stage 1: Add hardcoded story output

--- src/MadLib.java
+++ src/MadLib.java
@@ -3,5 +3,6 @@ import static java.lang.System.out;
 public class MadLib {
     public static void main(String[] args) {
         out.println("Mad Lib!");
+        out.println("The red dog quickly ran toward the blue sedan.");
     }
 }
